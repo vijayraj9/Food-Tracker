74f7196bb8d5607ac1ebb81d0ebd9dfa52c2a3a1
Ishan Zuaim <ishanzuaim@gmail.com>
2022-08-27 19:17:51 +1000

    Added server rendering.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,25 @@
 const express = require("express");
-const app = express();
+const webpush = require("web-push");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const path = require("path");
 
+const app = express();
+app.use(express.static(path.join(__dirname, "build")));
+
+app.use(bodyParser.json());
+webpush.setVapidDetails(
+  "mailto:test@test.com",
+  process.env.PUBLIC_KEY,
+  process.env.PRIVATE_KEY
+);
+
+app.post("", (req, res) => {
+  const subs = req.body;
+  res.status(201).json({});
+
+  webpush.sendNotification(subs, JSON.stringify({ title: "Test" }));
+});
 app.get("/api", (req, res) => {
   res.json({ user: ["1", "2", "3"] });
 });
